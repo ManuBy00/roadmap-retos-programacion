e3775361db51424efd8b6fe5f95e5622139a4291
ManuBy00 <166822103+ManuBy00@users.noreply.github.com>
2024-08-05 14:40:07 +0200

    Update ManuBy00.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ManuBy00.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ManuBy00.py
@@ -1,6 +1,17 @@
-#https://www.python.org
-#Comentario de solo una línea: almohadilla (#) al inicio de la línea.
-"""comentario de varias  líneas:
+# https://www.python.org
+# Comentario de solo una línea: almohadilla (#) al inicio de la línea.
+"""
+comentario de varias  líneas:
 se puede hacer un comentario de varias líneas
-abriendo con tres dobles comillas (""") y cerrando el comentario
-de la misma forma en otra línea"""
+abriendo con tres dobles comillas (") y cerrando el comentario
+de la misma forma en otra línea
+"""
+variable_int = 1
+variable_float = 1.5
+variable_bool = True
+variable_bool = False
+variable_string = "hola buenas tardes esto es una cadena"
+
+MY_CONSTANT = "Sosio no me lo cambies que es una constante"
+
+print "Hello, Python!"
