331e7286e8fbf7a81a66a3e974f8e3cfbc6d5146
ManuBy00 <166822103+ManuBy00@users.noreply.github.com>
2024-08-05 14:03:52 +0200

    Update and rename ManuBy00 to ManuBy00.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ManuBy00.py
@@ -0,0 +1,6 @@
+#https://www.python.org
+#Comentario de solo una línea: almohadilla (#) al inicio de la línea.
+"""comentario de varias  líneas:
+se puede hacer un comentario de varias líneas
+abriendo con tres dobles comillas (""") y cerrando el comentario
+de la misma forma en otra línea"""
